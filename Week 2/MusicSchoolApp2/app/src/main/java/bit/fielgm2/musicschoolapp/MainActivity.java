9330de09334bb9ee0a148c36a1ab1182aa61485f
gregfield <gregfield1996@gmail.com>
2016-02-29 18:00:09 +1300

    Music School App
    Music school app with spinner

--- Week 2/MusicSchoolApp2/app/src/main/java/bit/fielgm2/musicschoolapp/MainActivity.java
+++ Week 2/MusicSchoolApp2/app/src/main/java/bit/fielgm2/musicschoolapp/MainActivity.java
@@ -11,9 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,12 +27,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //button on click
         Button enrolBtn = (Button) findViewById(R.id.enrol);
         enrolBtn.setOnClickListener(new getButton());
+
+        //populates the spinner with required fields
+        String[] months = {"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November", "December"};
+
+        Spinner monthSpinner = (Spinner) findViewById(R.id.monthSpinner);
+        int layoutId = android.R.layout.simple_spinner_item;
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(this, layoutId, months);
+        monthSpinner.setAdapter(monthAdapter);
     }
 
-    public class getButton implements OnClickListener
-    {
+    public class getButton implements OnClickListener {
         @Override
         public void onClick(View v) {
 
@@ -39,9 +51,13 @@ public class MainActivity extends AppCompatActivity {
             RadioButton chosenBtn = (RadioButton) findViewById(instrumentGroup.getCheckedRadioButtonId());
             //gets the text of the checked radio button
             String instrumentChosen = chosenBtn.getText().toString();
+            //gets month
+            Spinner getMonth = (Spinner) findViewById(R.id.monthSpinner);
+            String month = getMonth.getSelectedItem().toString();
             //writes the instrument to the text view
             TextView chosenInstrumentText = (TextView) findViewById(R.id.enrolmentConfirm);
-            chosenInstrumentText.setText("You have enroled for " + instrumentChosen + " lessons");
+            chosenInstrumentText.setText("You have enroled for " + instrumentChosen + " lessons in " + month);
         }
     }
+
 }
