1bd6a747a65038459fa4a1f6a1b7d385994c262c
gregfield <gregfield1996@gmail.com>
2016-03-28 22:22:19 +1300

    Title Screen Layout Finished

--- Week 6/Language Trainer/app/src/main/java/bit/fielgm2/languagetrainer/TitleActivity.java
+++ Week 6/Language Trainer/app/src/main/java/bit/fielgm2/languagetrainer/TitleActivity.java
@@ -1,7 +1,10 @@
 package bit.fielgm2.languagetrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        RelativeLayout clickableLayout = (RelativeLayout) findViewById(R.id.titleScreenLayout);
+        clickableLayout.setOnClickListener(new layoutClick());
+    }
+
+    public class layoutClick implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            Intent startQuizIntent = new Intent(TitleActivity.this, QuizQuestionsActivity.class);
+            startActivity(startQuizIntent);
+        }
     }
 }
