9ea62dc043e4bfcaebec25f3884ca0e6ba42d4b6
gregfield <gregfield1996@gmail.com>
2016-04-13 14:45:49 +1200

    add internet permissions to manifest

--- Week 9/LastFmWebService/app/src/main/java/bit/fielgm2/lastfmwebservice/MainActivity.java
+++ Week 9/LastFmWebService/app/src/main/java/bit/fielgm2/lastfmwebservice/MainActivity.java
@@ -1,7 +1,11 @@
 package bit.fielgm2.lastfmwebservice;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public class getTopArtists extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String JSONString = "";
+
+            try {
+            }catch (Exception e){
+            }
+
+            return JSONString;
+        }
     }
 }
+
